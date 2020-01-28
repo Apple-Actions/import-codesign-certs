d4ee2f944de8472794de0df2658f0ada127a9768
Oliver Jones <oliver@deeperdesign.com>
2020-01-28 18:38:26 +1100

    Fix up test formatting

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,17 +1,18 @@
 import * as security from '../src/security'
 
 test('imports p12 file', async () => {
-  const keychain: string = "signing_temp"
+  const keychain: string = 'signing_temp'
   const keychainPassword: string = Math.random().toString(36)
-  const p12FilePath: string = "Certificates.p12"
-  const p12Password: string = "password"
+  const p12FilePath: string = 'Certificates.p12'
+  const p12Password: string = 'password'
 
   await security.installCertIntoTemporaryKeychain(
     keychain,
     true,
     keychainPassword,
     p12FilePath,
-    p12Password)
+    p12Password
+  )
 
   await security.deleteKeychain(keychain)
 })
